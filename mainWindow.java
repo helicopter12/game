191dcde152dc9c3c3e9a36edf130085e2f7587b0
helicopter12 <ryanstjohn3@gmail.com>
2016-01-14 16:07:58 -0500

    Updated mainWindow - Now handles the image fetching

--- mainWindow.java
+++ mainWindow.java
@@ -2,29 +2,44 @@
 //Authors: Ryan St. John, John Thornto
 //Date: 1/14/16
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
+import java.net.URL;
 
 public class mainWindow extends JFrame{
-    public static  boolean switchy = true;
+    public static boolean showStartMenu = true;
+    public static final int maxWidth = 1000;
+    public static final int maxHeight = 500;
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
-        JComponent pan = new paintPanel();
-        f.setTitle("Test");
-        f.setSize(500,500);
-        f.setLocation(25,25);
-        f.setResizable(true);
-        pan.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER,0), "enter");
-        pan.getActionMap().put("enter", new keyBinding("enter"));
-        f.getContentPane().add(pan);
-        f.setVisible(true);
 
-        while(switchy){
-            f.repaint();
+        if (downloadBackgroundImage()) {
+            JFrame f = new JFrame();
+            JComponent pan = new paintPanel();
+            f.setTitle("Test");
+            f.setSize(maxWidth, maxHeight);
+            f.setResizable(false);
+            pan.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter");
+            pan.getActionMap().put("enter", new keyBinding("enter"));
+            f.getContentPane().add(pan);
+            f.setVisible(true);
+
+            while (showStartMenu) {
+                f.repaint();
+            }
+        }
+    }
+
+    private static boolean downloadBackgroundImage() {
+        try {
+            paintPanel.backgroundImage = ImageIO.read(new URL("http://elder.comlu.com/game/space.jpg"));
+            return true;
+        } catch (java.io.IOException ex) {
+            return false;
         }
     }
 }
