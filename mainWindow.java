1080e1588e05dea539f559c706bb257f249fefea
helicopter12 <ryanstjohn3@gmail.com>
2016-01-14 11:00:42 -0500

    Created mainWindow file
    Added the main java file to the repository.

---
+++ mainWindow.java
@@ -0,0 +1,61 @@
+//Title: 2D Java Spaceship Game
+//Authors: Ryan St. John, John Thornto
+//Date: 1/14/16
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+
+
+public class mainWindow extends JFrame implements MouseListener, KeyListener {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setTitle("Test");
+        f.setSize(500,500);
+        f.setLocation(25,25);
+        f.setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent m) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent m) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent m) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent m) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent m) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent k) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent k) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent k) {
+
+    }
+}
