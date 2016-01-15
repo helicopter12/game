eab576e483aaa247c940962f6c730e733de0cff4
helicopter12 <ryanstjohn3@gmail.com>
2016-01-15 09:15:11 -0500

    Update keyBinding.java

--- keyBinding.java
+++ keyBinding.java
@@ -2,6 +2,7 @@ import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
 public class keyBinding extends AbstractAction {
+    private final int sensitivity = 10;
 
     private String cmd;
 
@@ -13,6 +14,14 @@ public class keyBinding extends AbstractAction {
     public void actionPerformed(ActionEvent e) {
         if (cmd.equalsIgnoreCase("enter")) {
             mainWindow.showStartMenu = false;
+        }else if(cmd.equalsIgnoreCase("w")){
+            localPlayer.location.y = localPlayer.location.y - sensitivity;
+        }else if(cmd.equalsIgnoreCase("a")){
+            localPlayer.location.x = localPlayer.location.x - sensitivity;
+        }else if(cmd.equalsIgnoreCase("s")){
+            localPlayer.location.y = localPlayer.location.y + sensitivity;
+        }else if(cmd.equalsIgnoreCase("d")){
+            localPlayer.location.x = localPlayer.location.x + sensitivity;
         }
     }
 }
