589323287a36cdcca0ee40d4d1552b7850470e4f
helicopter12 <ryanstjohn3@gmail.com>
2016-01-16 13:05:54 -0500

    Movement Update
    This class now handles local player movement

--- localPlayer.java
+++ localPlayer.java
@@ -2,4 +2,22 @@ import java.awt.Point;
 
 public class localPlayer {
     public static Point location = new Point (0,0);
+    private static final int sensitivity = 1;
+
+    public static void moveLocalPlayer()
+    {
+        final String keys = keyBinding.getKeysAsString();
+        if(keys.contains("a")){
+            location.x = location.x - sensitivity;
+        }
+        if(keys.contains("s")){
+            location.y = location.y + sensitivity;
+        }
+        if(keys.contains("w")){
+            location.y = location.y - sensitivity;
+        }
+        if(keys.contains("d")){
+            location.x = location.x + sensitivity;
+        }
+    }
 }
