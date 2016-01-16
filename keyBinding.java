4ba1cf23d05600c1a6aaf992603c6028de77b172
helicopter12 <ryanstjohn3@gmail.com>
2016-01-16 13:05:21 -0500

    Major Fixes to movement
    Now the player can move in two directions at once

--- keyBinding.java
+++ keyBinding.java
@@ -1,10 +1,10 @@
 import javax.swing.AbstractAction;
+import java.awt.List;
 import java.awt.event.ActionEvent;
 
 public class keyBinding extends AbstractAction {
-    private final int sensitivity = 10;
-
     private String cmd;
+    public static List keysDown = new List();
 
     public keyBinding(String cmd) {
         this.cmd = cmd;
@@ -12,16 +12,76 @@ public class keyBinding extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         if (cmd.equalsIgnoreCase("enter")) {
             mainWindow.showStartMenu = false;
-        }else if(cmd.equalsIgnoreCase("w")){
-            localPlayer.location.y = localPlayer.location.y - sensitivity;
-        }else if(cmd.equalsIgnoreCase("a")){
-            localPlayer.location.x = localPlayer.location.x - sensitivity;
-        }else if(cmd.equalsIgnoreCase("s")){
-            localPlayer.location.y = localPlayer.location.y + sensitivity;
-        }else if(cmd.equalsIgnoreCase("d")){
-            localPlayer.location.x = localPlayer.location.x + sensitivity;
+        }else if (cmd.equalsIgnoreCase("fire")) {
+            if (!listContains("fire")) {
+                keysDown.add("fire");
+            }
+        }else if (cmd.equalsIgnoreCase("rocket")) {
+            if (!listContains("rocket")) {
+                keysDown.add("rocket");
+            }
+        }else if (cmd.equalsIgnoreCase("w")){
+            if (!listContains("w")) {
+                keysDown.add("w");
+            }
+        }else if (cmd.equalsIgnoreCase("a")) {
+            if (!listContains("a")) {
+                keysDown.add("a");
+            }
+        }else if (cmd.equalsIgnoreCase("s")) {
+            if (!listContains("s")) {
+                keysDown.add("s");
+            }
+        }else if (cmd.equalsIgnoreCase("d")) {
+            if (!listContains("d")) {
+                keysDown.add("d");
+            }
+        }else if (cmd.equalsIgnoreCase("dr")) {
+            if (listContains("d")) {
+                keysDown.remove("d");
+            }
+        }else if (cmd.equalsIgnoreCase("ds")) {
+            if (listContains("s")) {
+                keysDown.remove("s");
+            }
+        }else if (cmd.equalsIgnoreCase("da")) {
+            if (listContains("a")) {
+                keysDown.remove("a");
+            }
+        }else if (cmd.equalsIgnoreCase("dw")) {
+            if (listContains("w")) {
+                keysDown.remove("w");
+            }
+        }else if (cmd.equalsIgnoreCase("dfire")) {
+            if (listContains("fire")) {
+                keysDown.remove("fire");
+            }
+        }else if (cmd.equalsIgnoreCase("drocket")) {
+            if (listContains("rocket")) {
+                keysDown.remove("rocket");
+            }
         }
+    }
+
+    private boolean listContains(String check) {
+        for (String Item : keysDown.getItems()) {
+            if(check.equals(Item)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String getKeysAsString() {
+        String ret = "";
+
+        for( String item : keysDown.getItems() ) {
+            ret += item;
+        }
+
+        return ret;
     }
 }
