6131107c7492d74dca474b396b22b068770b967a
helicopter12 <ryanstjohn3@gmail.com>
2016-01-16 13:07:09 -0500

    Updated Paint to show key presses
    Paint displays key presses 
    Background scrolling updated

--- paintPanel.java
+++ paintPanel.java
@@ -1,8 +1,9 @@
+import com.sun.org.apache.xpath.internal.operations.Gt;
+
 import javax.swing.JComponent;
 import java.awt.Image;
 import java.awt.Graphics;
 import java.awt.Color;
-
 
 public class paintPanel extends JComponent {
     public static Image backgroundImage, playerShip;
@@ -10,12 +11,18 @@ public class paintPanel extends JComponent {
     public double rocketSpeedPercent = 100;
 
     public void paint(Graphics g) {
-        //Draw the moving background - (Need a background that can be repeated)
-        g.drawImage((backgroundImage == null ? null : backgroundImage),(int)(stepTick <= (mainWindow.maxWidth - backgroundImage.getWidth(null)) ? stepTick = 0 : (stepTick = stepTick - ((rocketSpeedPercent/10000.0) * 5))), -10, null);
 
+        //Draw the moving background - (Need a background that can be repeated)
+        g.drawImage((backgroundImage == null ? null : backgroundImage),(int)(stepTick <= (mainWindow.maxWidth - backgroundImage.getWidth(null)) ? stepTick = 0 : (stepTick = stepTick - ((rocketSpeedPercent/1000.0) * 5))), -10, null);
+
+        //Draw the local player spaceship
         g.drawImage((playerShip == null ? null : playerShip), localPlayer.location.x, localPlayer.location.y, null);
 
+        //Draw the start menu
         drawStartMenu(g);
+
+        //Draw the keys down for debugging
+        drawKeysDown(g);
     }
 
     public void drawStartMenu(Graphics g) {
@@ -24,6 +31,16 @@ public class paintPanel extends JComponent {
             g.drawString("Space Ride v1.0", (mainWindow.maxWidth / 2) - 50, (mainWindow.maxHeight / 2) - 10);
             g.setColor(Color.white);
             g.drawString("Press 'Enter' to play",(mainWindow.maxWidth / 2) - 60, (mainWindow.maxHeight / 2) + 10);
+        }
+    }
+
+    public void drawKeysDown(Graphics g) {
+        if(keyBinding.keysDown.toString() != null) {
+            int i = 20;
+            for (String member : keyBinding.keysDown.getItems()) {
+                g.drawString(member, 10, i);
+                i+=20;
+            }
         }
     }
 }
