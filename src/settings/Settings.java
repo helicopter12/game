3a1f553a5c458d75577b072358706c71109fd72c
jthortcode <jthornton240@gmail.com>
2016-01-17 02:42:53 -0500

    Skeleton Updated

---
+++ src/settings/Settings.java
@@ -0,0 +1,9 @@
+package settings;
+
+public class Settings {
+	
+	public static boolean enableInternalErrors = false;
+	public static int windowWidth = 700;
+	public static int windowHeight = 700;
+	
+}
